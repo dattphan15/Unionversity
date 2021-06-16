a53418ef2e19a47c25ecf78e80e8beebdcfe8fa7
dattphan15 <ken.phan@live.ca>
2021-06-15 21:51:49 -0700

    type definition for StudyGroup

--- index.ts
+++ index.ts
@@ -8,3 +8,11 @@ type Course = {
   keywords: string[];
   eventType: string;
 }
+
+type StudyGroup = {
+  id: number;
+  courseId: number;
+  title: string;
+  keywords: string;
+  eventType: string;
+}
